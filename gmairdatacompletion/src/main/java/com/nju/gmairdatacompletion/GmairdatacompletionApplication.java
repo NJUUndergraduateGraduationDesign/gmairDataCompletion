0f165667fad6eea9456145f153d4b31d387ca759
ck <578045968@qq.com>
2020-02-07 17:26:02 +0800

    初始化

---
+++ gmairdatacompletion/src/main/java/com/nju/gmairdatacompletion/GmairdatacompletionApplication.java
@@ -0,0 +1,13 @@
+package com.nju.gmairdatacompletion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmairdatacompletionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmairdatacompletionApplication.class, args);
+    }
+
+}
---
+++ gmairdatacompletion/src/test/java/com/nju/gmairdatacompletion/GmairdatacompletionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nju.gmairdatacompletion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmairdatacompletionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
