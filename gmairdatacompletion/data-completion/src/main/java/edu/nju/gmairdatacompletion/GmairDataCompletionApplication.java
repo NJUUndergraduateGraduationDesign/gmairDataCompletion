2912a794e43fe31d7a1f19b6cb38b8ffdef7b167
tsl19972007 <498656288@qq.com>
2020-02-21 14:49:13 +0800

    调整项目模块结构

--- gmairdatacompletion/data-completion/src/main/java/edu/nju/gmairdatacompletion/GmairDataCompletionApplication.java
+++ gmairdatacompletion/data-completion/src/main/java/edu/nju/gmairdatacompletion/GmairDataCompletionApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GmairdatacompletionApplication {
+public class GmairDataCompletionApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GmairdatacompletionApplication.class, args);
+        SpringApplication.run(GmairDataCompletionApplication.class, args);
     }
 
 }
