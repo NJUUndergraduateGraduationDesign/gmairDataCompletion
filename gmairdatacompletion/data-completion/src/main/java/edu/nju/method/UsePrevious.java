7719fa0c0dd54cf085bd7af5a92483de236f2cef
chenguangming <980231201@qq.com>
2020-02-26 12:24:04 +0800

    test.

--- gmairdatacompletion/data-completion/src/main/java/edu/nju/method/UsePrevious.java
+++ gmairdatacompletion/data-completion/src/main/java/edu/nju/method/UsePrevious.java
@@ -1,4 +1,19 @@
 package edu.nju.method;
 
+import edu.nju.model.MachinePartialStatus;
+import edu.nju.model.MachineV1Status;
+import edu.nju.model.MachineV2Status;
+import edu.nju.model.MachineV3Status;
+
+import java.util.List;
+
 public interface UsePrevious {
+
+    List<MachinePartialStatus> partialUsePrevious(List<MachinePartialStatus> selectedData);
+
+    List<MachineV1Status> v1UsePrevious(List<MachineV1Status> selectedData);
+
+    List<MachineV2Status> v2UsePrevious(List<MachineV2Status> selectedData);
+
+    List<MachineV3Status> v3UsePrevious(List<MachineV3Status> selectedData);
 }
--- gmairdatacompletion/data-completion/src/main/java/edu/nju/method/UsePreviousImpl.java
+++ gmairdatacompletion/data-completion/src/main/java/edu/nju/method/UsePreviousImpl.java
@@ -1,4 +1,30 @@
 package edu.nju.method;
 
+import edu.nju.model.MachinePartialStatus;
+import edu.nju.model.MachineV1Status;
+import edu.nju.model.MachineV2Status;
+import edu.nju.model.MachineV3Status;
+
+import java.util.List;
+
 public class UsePreviousImpl implements UsePrevious {
+    @Override
+    public List<MachinePartialStatus> partialUsePrevious(List<MachinePartialStatus> selectedData) {
+        return null;
+    }
+
+    @Override
+    public List<MachineV1Status> v1UsePrevious(List<MachineV1Status> selectedData) {
+        return null;
+    }
+
+    @Override
+    public List<MachineV2Status> v2UsePrevious(List<MachineV2Status> selectedData) {
+        return null;
+    }
+
+    @Override
+    public List<MachineV3Status> v3UsePrevious(List<MachineV3Status> selectedData) {
+        return null;
+    }
 }
